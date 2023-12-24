849589cb1cb87dbbc795e9924ec6e65cb1b2f921
Starrism <hyw-luv@zju.edu.cn>
2023-12-24 17:15:35 +0800

    update: fix module "food"

--- src/food.v
+++ src/food.v
@@ -7,17 +7,14 @@ module food (
     output reg [4:0] food_x,
     output reg [4:0] food_y
 );
-    always @(posedge clk)
-    begin
-        if(rst == 1) begin
-            food_x <= 5'd0;
-            food_y <= 5'd0;
-        end
-        else if(game_state == 2'b01) begin
-            if(get_food == 1) begin
-                food_x <= {$random} % 32;
-                food_y <= {$random} % 24;
-            end
+    if(rst == 1) begin
+        food_x <= 5'd0;
+        food_y <= 5'd0;
+    end
+    else if(game_state == 2'b01) begin
+        if(get_food == 1) begin
+            food_x <= {$random} % 32;
+            food_y <= {$random} % 24;
         end
     end
 endmodule

7c28e4af0a2f0463448c8f70294cad2ad2f624bc
Starrism <hyw-luv@zju.edu.cn>
2024-01-02 13:35:30 +0800

    fix: fix random generator

--- src/food.v
+++ src/food.v
@@ -6,10 +6,13 @@ module food (
     output reg [4:0] food_x,
     output reg [4:0] food_y
 );
+    reg [6:0] cnt;
     always @(posedge clk) begin
+        if (cnt < 96) cnt <= cnt + 1;
+        else cnt <= 0;
         if (game_state == 2'b10 || game_state == 2'b00 && get_food) begin
-            food_x <= {$random} % 32;
-            food_y <= {$random} % 24;
+            food_x <= cnt % 32;
+            food_y <= cnt % 24;
         end
     end
 

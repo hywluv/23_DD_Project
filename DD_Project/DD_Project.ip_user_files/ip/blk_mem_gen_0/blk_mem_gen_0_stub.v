e51f6a80b99a2a320d6e6b4466c61976957b43d8
Starrism <hyw-luv@zju.edu.cn>
2024-01-03 14:11:45 +0800

    update：vivado project

---
+++ DD_Project/DD_Project.ip_user_files/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+// Date        : Tue Jan  2 19:15:52 2024
+// Host        : xyw-luv running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               d:/DD_exp/23_DD_Project/DD_Project/DD_Project.gen/sources_1/ip/blk_mem_gen_0_1/blk_mem_gen_0_stub.v
+// Design      : blk_mem_gen_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7k160tffg676-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_6,Vivado 2023.1" *)
+module blk_mem_gen_0(clka, wea, addra, dina, douta)
+/* synthesis syn_black_box black_box_pad_pin="wea[0:0],addra[18:0],dina[11:0],douta[11:0]" */
+/* synthesis syn_force_seq_prim="clka" */;
+  input clka /* synthesis syn_isclock = 1 */;
+  input [0:0]wea;
+  input [18:0]addra;
+  input [11:0]dina;
+  output [11:0]douta;
+endmodule
